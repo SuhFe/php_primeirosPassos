b45cb74708bf457ca573f880e2d9f04af54a7947
Suzana <su_zana-santos@hotmail.com>
2023-11-16 19:28:21 -0300

    Condicional

--- condicional-2.php
+++ condicional-2.php
@@ -6,3 +6,10 @@ for ($contador = 1; $contador <= 30; $contador++) {
 }
    echo "N° $contador" . PHP_EOL;
 }
+
+/*
+Existem algumas variações de laços, dentre elas:
+while: que possui uma condição de entrada
+do-while: que possui uma condição de permanência (sempre executa o bloco pelo menos uma vez)
+for: que possui a declaração de uma variável, uma condição entrada e o increment
+*/
