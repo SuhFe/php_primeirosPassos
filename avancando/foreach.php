f27db30cfed1840d25757ae2a12b2d6331518b86
Suzana <su_zana-santos@hotmail.com>
2023-11-18 10:13:54 -0300

    Foreach

---
+++ avancando/foreach.php
@@ -0,0 +1,18 @@
+<?php
+
+$conta1 = [
+    'titular'=> 'Vinicius', 
+    'saldo'=> 1000
+];
+
+$conta2 = [
+    'titular'=> 'Suzana', 
+    'saldo'=> 5000
+];
+
+$conta3 = [
+    'titular'=> 'Paulo', 
+    'saldo'=> 500
+];
+
+echo $conta1['titular'];
