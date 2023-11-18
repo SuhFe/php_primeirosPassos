51761d1a9d127b194ddca81d1f70e72f73cc6007
Suzana <su_zana-santos@hotmail.com>
2023-11-18 10:53:39 -0300

    Adicionando elementos ao final de  arrays

--- avancando/loop.php
+++ avancando/loop.php
@@ -2,6 +2,10 @@
 
 $idadeList = [21, 23, 19, 25, 30, 41, 18, 78];
 
+$idadeList[] = 9;
+$idadeList[] = 3;
+$idadeList[] = 6;
+
 for ($i = 0; $i < count($idadeList) ; $i++) { 
     echo $idadeList[$i] . PHP_EOL;
 } 
