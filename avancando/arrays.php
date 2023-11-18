c6e268c6cfae67f4db434096ff73ceee14e6f81d
Suzana <su_zana-santos@hotmail.com>
2023-11-18 10:10:52 -0300

    Atualizações

--- avancando/arrays.php
+++ avancando/arrays.php
@@ -2,7 +2,19 @@
 
 $conta1 = [
     'titular'=> 'Vinicius', 
-    'saldo'=>1000
+    'saldo'=> 1000
 ];
+
+$conta2 = [
+    'titular'=> 'Suzana', 
+    'saldo'=> 5000
+];
+
+$conta3 = [
+    'titular'=> 'Paulo', 
+    'saldo'=> 500
+];
+
+echo $conta1['titular'];
 
 $contasCorrentes = [$conta1, $conta2, $conta3];
