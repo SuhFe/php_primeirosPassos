98332d20ea156561f67f281b231759d6a361d4eb
Suzana <su_zana-santos@hotmail.com>
2023-11-19 02:07:48 -0300

    Fazendo operações

--- avancando/banco.php
+++ avancando/banco.php
@@ -2,26 +2,27 @@
 
 $contasCorrentes = [
     '123.456.789-10' => [
-        'titular' => 'Vinicius',
-        'saldo' => 2000
-    ],
-    '429.298.418-26' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ],
-    '123.456.789-12'=> [
-    'titular' => 'Alberto',
-    'saldo' => 300
+    '123.456.789-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ],
+    '123.256.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
     ]
 ];
 
-$contasCorrentes['429.298.418-26']['saldo'] -= 500;
-if (500 < $contasCorrentes['123.456.789-10']['saldo']) {
-   echo "Seu saldo é insuficiente";
-}
-
 $contasCorrentes['123.456.789-10']['saldo'] -= 500;
+if (500 > $contasCorrentes['123.456.789-11']['saldo']) {
+    echo "Você não pode sacar este valor" . PHP_EOL;
+} else 
+    $contasCorrentes['123.456.789-11']['saldo'] -= 500;
+
+$contasCorrentes['123.456.789-11']['saldo'] -= 500;
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    echo $cpf . " " . $conta['titular'] . " = " . $conta['saldo'] . PHP_EOL; 
-};
+    echo $cpf . " " . $conta['titular'] . " = " . $conta['saldo'] . PHP_EOL;
+}
